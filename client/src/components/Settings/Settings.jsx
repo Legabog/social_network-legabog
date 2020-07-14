4a4437aac03aa89a0d26a85f40ae70c4fb0b8d84
legabog <probringer@mail.ru>
2020-07-14 19:28:31 +0300

    Add files via upload

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -28,9 +28,48 @@ const Settings = (props) => {
           <button
             className={classes.categoriesbuttons}
             onClick={() => {
-              Axios.put(`http://localhost:4000/musicalbums//update-album/5ed8c632b939c242b024f4bc`, {
-                albumcoverUrl: "https://avatars.yandex.net/get-music-content/2808981/865b055d.a.10258179-1/200x200"
-              })
+              Axios.put(
+                `http://localhost:4000/musicalbums//update-album/5f06e3754da31a27f86836f4`,
+                {
+                  "tracks": [
+                    {
+                      "_id": "5f06e3754da31a27f86836f5",
+                      "title": "Вторник",
+                      "trackUrl": "https://firebasestorage.googleapis.com/v0/b/legabog-feedback-app.appspot.com/o/music-storage%2FSALUKI%2F%D0%9D%D0%B0%20%D0%A7%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0%2Fsaluki_%D0%B2%D1%82%D0%BE%D1%80%D0%BD%D0%B8%D0%BA.mp3?alt=media"
+                    },
+                    {
+                      "_id": "5f06e3754da31a27f86836f6",
+                      "title": "Вкус Твоих Слёз",
+                      "trackUrl": "https://firebasestorage.googleapis.com/v0/b/legabog-feedback-app.appspot.com/o/music-storage%2FSALUKI%2F%D0%9D%D0%B0%20%D0%A7%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0%2Fsaluki_%D0%B2%D0%BA%D1%83%D1%81_%D1%82%D0%B2%D0%BE%D0%B8%D1%85_%D1%81%D0%BB%D1%91%D0%B7.mp3?alt=media"
+                    },
+                    {
+                      "_id": "5f06e3754da31a27f86836f7",
+                      "title": "Кома",
+                      "trackUrl": "https://firebasestorage.googleapis.com/v0/b/legabog-feedback-app.appspot.com/o/music-storage%2FSALUKI%2F%D0%9D%D0%B0%20%D0%A7%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0%2Fsaluki_%D0%BA%D0%BE%D0%BC%D0%B0.mp3?alt=media"
+                    },
+                    {
+                      "_id": "5f06e3754da31a27f86836f8",
+                      "title": "0,33",
+                      "trackUrl": "https://firebasestorage.googleapis.com/v0/b/legabog-feedback-app.appspot.com/o/music-storage%2FSALUKI%2F%D0%9D%D0%B0%20%D0%A7%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0%2Fsaluki_0%2C33.mp3?alt=media"
+                    },
+                    {
+                      "_id": "5f06e3754da31a27f86836f9",
+                      "title": "На Человека",
+                      "trackUrl": "https://firebasestorage.googleapis.com/v0/b/legabog-feedback-app.appspot.com/o/music-storage%2FSALUKI%2F%D0%9D%D0%B0%20%D0%A7%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0%2Fsaluki_%D0%BD%D0%B0_%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0.mp3?alt=media"
+                    },
+                    {
+                      "_id": "5f06e3754da31a27f86836fa",
+                      "title": "Лёд",
+                      "trackUrl": "https://firebasestorage.googleapis.com/v0/b/legabog-feedback-app.appspot.com/o/music-storage%2FSALUKI%2F%D0%9D%D0%B0%20%D0%A7%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0%2Fsaluki_%D0%BB%D1%91%D0%B4.mp3?alt=media"
+                    },
+                    {
+                      "_id": "5f06e3754da31a27f86836fb",
+                      "title": "777",
+                      "trackUrl": "https://firebasestorage.googleapis.com/v0/b/legabog-feedback-app.appspot.com/o/music-storage%2FSALUKI%2F%D0%9D%D0%B0%20%D0%A7%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0%2Fsaluki_777.mp3?alt=media"
+                    }
+                  ],
+                }
+              );
             }}
           >
             business
